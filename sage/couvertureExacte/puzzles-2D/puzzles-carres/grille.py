800952fb94ad582175c33f3e454452c65e20e109
YvesLemaire <y.l31416@gmail.com>
2024-04-01 17:31:10 +0200

    Add files via upload

--- sage/couvertureExacte/puzzles-2D/puzzles-carres/grille.py
+++ sage/couvertureExacte/puzzles-2D/puzzles-carres/grille.py
@@ -9,13 +9,15 @@ ax.axis('off')
 ax.axis('equal')
 ax.axis([-3,3,-3,3])
 
-for u in range(-3,3):
-    ax.plot([u + .5 for _ in range(6)], [y + .5 for y in range(-3,3)], color = 'grey')
-    ax.plot([x + .5 for x in range(-3,3)], [u + .5 for _ in range(6)], color = 'grey')
-for i in range(-2,3):
-    for j in range(-2,3):
+for u in range(-1,3):
+    ax.plot([u + .5 for _ in range(3)], [y + .5 for y in range(-1,2)], color = 'grey')
+    if u < 2:
+        ax.plot([x + .5 for x in range(-1,3)], [u + .5 for _ in range(4)], color = 'grey')
+for i in range(3):
+    for j in range(2):
         ax.plot(i,j, color = 'black', marker = 'o', markersize = '2')
         ax.text(i, j - .2, f'${i},{j}$', fontsize = 11, color = 'black', verticalalignment = 'top', horizontalalignment = 'center')
 
+#plt.show()
 
 plt.savefig('grille.png')
